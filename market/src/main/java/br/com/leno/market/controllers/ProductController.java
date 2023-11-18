caa62d07f1f6abb72f7109b7baf410ff3178dfb1
Girleno Lima <girlenosilveira@gmail.com>
2023-11-18 12:00:34 -0300

    update end

--- market/src/main/java/br/com/leno/market/controllers/ProductController.java
+++ market/src/main/java/br/com/leno/market/controllers/ProductController.java
@@ -1,6 +1,5 @@
 package br.com.leno.market.controllers;
 
-
 import br.com.leno.market.dtos.ProductRecordDto;
 import br.com.leno.market.models.ProductModel;
 import br.com.leno.market.repositories.ProductRepository;
@@ -10,11 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 /*
  * @RestController é derivada da @Controoler
@@ -60,8 +60,15 @@ public class ProductController {
     @GetMapping("/products")
     public ResponseEntity<List<ProductModel>> getAllProducts() {
 
-        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+        List<ProductModel> productModelList = productRepository.findAll();
+        if(!productModelList.isEmpty()){
+            for (ProductModel item: productModelList ) {
+                UUID id = item.getIdProduct();
+                item.add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withSelfRel());
+            }
+        }
 
+        return ResponseEntity.status(HttpStatus.OK).body(productModelList);
     }
 
     /*
@@ -74,13 +81,53 @@ public class ProductController {
 
         Optional<ProductModel> product1 = productRepository.findById(id);
         if (product1.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not foubd");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(product1.get());
 
 
     }
+
+    @PutMapping ("/products/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id,
+                                                @RequestBody @Valid ProductRecordDto productRecordDto) {
+
+        //verifica se o recurso existe (ID)
+        Optional<ProductModel> product1 = productRepository.findById(id);
+        if (product1.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not foubd");
+        }
+
+        // Se ele existir salva ele na variavel
+        var productModel = product1.get();
+
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+
+
+    }
+
+    @DeleteMapping ("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id) {
+
+        //verifica se o recurso existe (ID)
+        Optional<ProductModel> product1 = productRepository.findById(id);
+        if (product1.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not foubd");
+        }
+
+
+        productRepository.delete(product1.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Produto deletado");
+
+
+    }
+
+
+
+
+
 
 
 }
